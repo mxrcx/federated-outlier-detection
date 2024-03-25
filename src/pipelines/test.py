85eb8903f96d21f6edf8d1a7908cc6cae481a0b3
Marco Schaarschmidt <marco.schaarschmidt@student.hpi.de>
2024-03-25 16:54:44 +0100

    Update test script

--- src/pipelines/test.py
+++ src/pipelines/test.py
@@ -1,8 +1,26 @@
 from data.loading import load_configuration, load_parquet
 
-path, filename, config_settings = load_configuration()
-data = load_parquet(path["interim"], filename["combined"], optimize_memory=True)
-features = load_parquet(path["features"], filename["features"], optimize_memory=True)
 
-print(data.head())
-print(features.head())
+def print_shapes():
+    path, filename, _config_settings = load_configuration()
+    original_data = load_parquet(
+        path["interim"], filename["combined"], optimize_memory=True
+    )
+    features = load_parquet(
+        path["features"], filename["features"], optimize_memory=True
+    )
+    processed_data = load_parquet(
+        path["processed"], filename["processed"], optimize_memory=True
+    )
+
+    print(f"Original data shape: {original_data.shape}")
+    print(f"Features shape: {features.shape}")
+    print(f"Processed data shape: {processed_data.shape}")
+
+
+def run_test():
+    print_shapes()
+
+
+if __name__ == "__main__":
+    run_test()
